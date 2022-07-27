7796ef49eab123e3f41a4660590dc1b3a41805c0
Gerald <gm@gerald.host>
2022-07-28 00:26:10 +0100

    feat: added super basic check for opcodes

---
+++ README.md
@@ -0,0 +1,26 @@
+# Troy
+
+The EVM foot soldier
+
+## Usage
+
+```
+$ troy -a <CONTRACT_ADDRESS> -k <INFURA_ID> -n <NETWORK>
+$ troy -a 0x6b175474e89094c44da98b954eedeac495271d0f -k <INFURA_ID> -n mainnet
+
+```
+
+```
+$ troy -c <BYTE_CODE>
+$ troy -c 60ff60ff
+```
+
+## Notes
+
+Ideas of some things we can scan for initially
+
+- Reentrency
+  - detect CALL and DELEGATECALL before sstore
+- Highlight notable opcodes CALL DELEGATECALL CHAINID
+- Highlight notable function calls "approve", "mint", "transfer"
+  - detect if they have a CALLER comparison with EQ and JUMPI
--- src/main.go
+++ src/main.go
@@ -9,6 +9,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var notableOps = map[byte]bool{
+	0xf0: true, // CREATE
+	0xf1: true, // CALL
+	0xf4: true, // DELEGATECALL
+	0xf5: true, // CREATE2
+	0xff: true, // SELFDESTRUCT
+	0x63: true, // PUSH4
+}
+
 func Start() {
 	var code []byte
 
@@ -34,10 +43,20 @@ func Start() {
 
 	it := dasm.NewInstructionIterator(code)
 	for it.Next() {
+		if _, ok := notableOps[byte(it.Op())]; ok {
+			// TODO: make a seperate function to parse op codes to highlight anything
+			// notable and then pipe it into some nice terminal UI. Should be able to
+			// do more advanced lookups too like if the opcode is CALL look for future
+			// SSTORES. We can also look at the Arg of CALL to highlight interesting
+			// function sigs like approve, mint, transfer etc.
+			log.Info(fmt.Sprintf("Noteable opcode found: %v", it.Op()))
+		}
+
 		if it.Arg() != nil && 0 < len(it.Arg()) {
 			fmt.Printf("%05x: %v %#x\n", it.PC(), it.Op(), it.Arg())
 		} else {
 			fmt.Printf("%05x: %v\n", it.PC(), it.Op())
 		}
 	}
+
 }
