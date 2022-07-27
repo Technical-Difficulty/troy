275951a0fde1fe879b880f2a9d635d3d7fb12856
mt-code <matt@mtcode.co.uk>
2022-07-27 22:41:51 +0100

    misc: Refactored instruction iterator into dasm package

--- src/main.go
+++ src/main.go
@@ -1,13 +1,32 @@
 package src
 
 import (
+	"encoding/hex"
 	"fmt"
+	"strings"
+	"troy/src/dasm"
 	"troy/src/eth"
 )
 
 func Start() {
 	config := InitConfig()
 	contract := eth.NewContract(config.Address, config.RPCUrl, config.Network)
+	code := contract.GetByteCode()
+	code = strings.Replace(code, "0x", "", -1)
 
-	fmt.Println(contract.GetByteCode())
+	fmt.Printf("%v\n", code)
+
+	script, err := hex.DecodeString(code)
+	if err != nil {
+		fmt.Println("Error decoding")
+	}
+
+	it := dasm.NewInstructionIterator(script)
+	for it.Next() {
+		if it.Arg() != nil && 0 < len(it.Arg()) {
+			fmt.Printf("%05x: %v %#x\n", it.PC(), it.Op(), it.Arg())
+		} else {
+			fmt.Printf("%05x: %v\n", it.PC(), it.Op())
+		}
+	}
 }
