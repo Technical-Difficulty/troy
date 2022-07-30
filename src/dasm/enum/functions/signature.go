eca3fef05e558828e0b166129936b3536da49c7b
mt-code <matt@mtcode.co.uk>
2022-07-31 00:23:43 +0100

    feat: initially attempt an offline lookup for quicker speeds

--- src/dasm/enum/functions/signature.go
+++ src/dasm/enum/functions/signature.go
@@ -3,7 +3,6 @@ package functions
 import (
 	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"io/ioutil"
 	"net/http"
 	"troy/src/dasm"
@@ -34,44 +33,46 @@ func (f *Signature) String() string {
 
 // todo: multi-thread this for online lookups
 func (f *Signature) Lookup(signatures *map[string][]string) (sigs []string) {
-	res, err := f.onlineLookup()
+	sigs = f.offlineLookup(signatures)
+
+	if sigs != nil {
+		return sigs
+	}
+
+	return f.onlineLookup()
+}
+
+func (f *Signature) onlineLookup() (sigs []string) {
+	response, err := http.Get("https://www.4byte.directory/api/v1/signatures/?hex_signature=" + f.String())
 
 	// todo: add logging here if err != nil, perhaps with a verbose flag
+	if err != nil {
+		return nil
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return nil
+	}
+
+	var res FourByteResponse
+	if err := json.Unmarshal(contents, &res); err != nil {
+		return nil
+	}
+
+	if res.Count <= 0 {
+		return nil
+	}
+
 	if err == nil {
 		for _, s := range res.Results {
 			sigs = append(sigs, s.TextSignature)
 		}
-
-		return sigs
 	}
 
-	return f.offlineLookup(signatures)
-}
-
-func (f *Signature) onlineLookup() (FourByteResponse, error) {
-	response, err := http.Get("https://www.4byte.directory/api/v1/signatures/?hex_signature=" + f.String())
-
-	if err != nil {
-		return FourByteResponse{}, err
-	} else {
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
-
-		if err != nil {
-			return FourByteResponse{}, err
-		}
-
-		var res FourByteResponse
-		if err := json.Unmarshal(contents, &res); err != nil {
-			return FourByteResponse{}, err
-		}
-
-		if res.Count <= 0 {
-			return FourByteResponse{}, errors.New("Not found")
-		}
-
-		return res, nil
-	}
+	return sigs
 }
 
 func (f *Signature) offlineLookup(signatures *map[string][]string) (sigs []string) {
