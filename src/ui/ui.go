9b6524f1520e0fcf66bc0030fabbbcf679a9647a
mt-code <matt@mtcode.co.uk>
2022-07-30 21:00:37 +0100

    refactor: move title set logic into flex create func

--- src/ui/ui.go
+++ src/ui/ui.go
@@ -17,12 +17,8 @@ func Start(contract eth.Contract, config src.Config) UI {
 	app := tview.NewApplication()
 	tbl := table.NewInstructionTable(contract.Instructions, config)
 
-	flex := createFlex()
+	flex := createFlex(contract)
 	flex.AddItem(tbl.View, 0, 1, true)
-
-	if !contract.IsMock {
-		flex.SetTitle(fmt.Sprintf("[ %s ]", contract.Address))
-	}
 
 	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
@@ -34,11 +30,16 @@ func Start(contract eth.Contract, config src.Config) UI {
 	}
 }
 
-func createFlex() *tview.Flex {
+func createFlex(contract eth.Contract) *tview.Flex {
 	flex := tview.NewFlex()
 	flex.SetDirection(tview.FlexRow)
-	flex.SetTitle("[ Troy ]")
 	flex.SetBorder(true)
+
+	if contract.IsMock {
+		flex.SetTitle("[ Troy ]")
+	} else {
+		flex.SetTitle(fmt.Sprintf("[ %s ]", contract.Address))
+	}
 
 	return flex
 }
